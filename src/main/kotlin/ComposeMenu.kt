7fef24bf4deaf949612a0d599332f96bb172b89d
Russell Banks <Russell@bandev.uk>
2021-12-14 13:43:25 +0000

    Add simple dropdown menu

---
+++ src/main/kotlin/ComposeMenu.kt
@@ -0,0 +1,73 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun ComposeMenu(
+    menuItems: List<String>,
+    menuExpandedState: Boolean,
+    seletedIndex : Int,
+    updateMenuExpandStatus : () -> Unit,
+    onDismissMenuView : () -> Unit,
+    onMenuItemclick : (Int) -> Unit,
+) {
+    Box(
+        modifier = Modifier
+            .wrapContentSize(Alignment.TopStart)
+            .padding(top = 10.dp)
+            .border(0.5.dp, MaterialTheme.colors.onSurface.copy(alpha = 0.5f))
+            .clickable(
+                onClick = {
+                    updateMenuExpandStatus()
+                },
+            ),
+
+        ) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+
+            val (lable, iconView) = FocusRequester.createRefs()
+
+            Text(
+                text = menuItems[seletedIndex],
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+
+            DropdownMenu(
+                expanded = menuExpandedState,
+                onDismissRequest = { onDismissMenuView() },
+                modifier = Modifier
+                    .wrapContentSize()
+                    .background(MaterialTheme.colors.surface)
+            ) {
+                menuItems.forEachIndexed { index, title ->
+                    DropdownMenuItem(
+                        onClick = {
+                            if (index != 0) {
+                                onMenuItemclick(index)
+                            }
+                        }) {
+                        Text(text = title)
+                    }
+                }
+            }
+        }
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,18 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.AwtWindow
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
@@ -26,11 +28,41 @@ fun App() {
     MaterialTheme {
         var isOpen by remember { mutableStateOf(false) }
         var hashedOutput by remember { mutableStateOf("No hashed file") }
+        var dropDownOpen by remember { mutableStateOf(false) }
+        var algorithm by remember { mutableStateOf(MessageDigestAlgorithm.MD5) }
+        var selectedIndex by remember { mutableStateOf(1) }
+        val menuItems = listOf(
+            MessageDigestAlgorithm.MD2,
+            MessageDigestAlgorithm.MD5,
+            MessageDigestAlgorithm.SHA_1,
+            MessageDigestAlgorithm.SHA_224,
+            MessageDigestAlgorithm.SHA_256,
+            MessageDigestAlgorithm.SHA_384,
+            MessageDigestAlgorithm.SHA_512,
+            MessageDigestAlgorithm.SHA_512_224,
+            MessageDigestAlgorithm.SHA_512_256,
+            MessageDigestAlgorithm.SHA3_224,
+            MessageDigestAlgorithm.SHA3_256,
+            MessageDigestAlgorithm.SHA3_384,
+            MessageDigestAlgorithm.SHA3_512
+        )
         Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            ComposeMenu(
+                menuItems = menuItems,
+                menuExpandedState = dropDownOpen,
+                seletedIndex = selectedIndex,
+                updateMenuExpandStatus = { dropDownOpen = !dropDownOpen},
+                onDismissMenuView = { dropDownOpen = false },
+                onMenuItemclick = {
+                    dropDownOpen = false
+                    algorithm = menuItems[it]
+                    selectedIndex = it
+                }
+            )
             Text(hashedOutput)
             Button(onClick = { isOpen = true }) { Text("File") }
         }
@@ -39,7 +71,7 @@ fun App() {
             FileDialog(
                 onCloseRequest = {
                     isOpen = false
-                    hashedOutput = HashUtils.getCheckSumFromFile(MessageDigest.getInstance(MessageDigestAlgorithm.MD5), it[0]).uppercase()
+                    hashedOutput = HashUtils.getCheckSumFromFile(MessageDigest.getInstance(algorithm), it).uppercase()
                 }
             )
         }
@@ -55,14 +87,14 @@ fun main() = application {
 @Composable
 private fun FileDialog(
     parent: Frame? = null,
-    onCloseRequest: (result: Array<File>) -> Unit
+    onCloseRequest: (result: File) -> Unit
 ) = AwtWindow(
     create = {
         object : FileDialog(parent, "Choose a file", LOAD) {
             override fun setVisible(value: Boolean) {
                 super.setVisible(value)
                 if (value) {
-                    onCloseRequest(files)
+                    onCloseRequest(files[0])
                 }
             }
         }

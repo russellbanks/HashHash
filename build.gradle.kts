0ecb8dfe6d6e5c39a450281c508f68dba834e3f5
Russell Banks <Russell@bandev.uk>
2021-12-17 17:27:04 +0000

    Add top section with information and file icon

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "16"
+    kotlinOptions.jvmTarget = "17"
 }
 
 compose.desktop {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,22 +4,22 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Add
-import androidx.compose.material.icons.rounded.Check
-import androidx.compose.material.icons.rounded.ContentCopy
-import androidx.compose.material.icons.rounded.ErrorOutline
+import androidx.compose.material.icons.rounded.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.AwtWindow
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
+import androidx.compose.ui.window.singleWindowApplication
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
@@ -28,9 +28,18 @@ import kotlinx.coroutines.launch
 import java.awt.FileDialog
 import java.awt.Frame
 import java.awt.Toolkit
+import java.awt.datatransfer.DataFlavor
 import java.awt.datatransfer.StringSelection
+import java.awt.dnd.DnDConstants
+import java.awt.dnd.DropTarget
+import java.awt.dnd.DropTargetAdapter
+import java.awt.dnd.DropTargetDropEvent
 import java.io.File
-import java.util.concurrent.TimeUnit
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.text.CharacterIterator
+import java.text.StringCharacterIterator
+import kotlin.math.abs
 
 lateinit var file: File
 
@@ -42,13 +51,17 @@ fun App() {
 
         var isFileManagerOpen by remember { mutableStateOf(false) }
         var hashedOutput by remember { mutableStateOf("") }
-        var providedHash by remember { mutableStateOf("") }
+        var comparisonHash by remember { mutableStateOf("") }
         var dropDownOpen by remember { mutableStateOf(false) }
         var algorithm by remember { mutableStateOf(Algorithms.MD5) }
         var selectedIndex by remember { mutableStateOf(1) }
         val snackbarHostState = remember { SnackbarHostState() }
         var hashTimer by remember { mutableStateOf("00:00") }
         var timerVisible by remember { mutableStateOf(false) }
+        var fileName by remember { mutableStateOf("File name") }
+        var fileType by remember { mutableStateOf("Type") }
+        var fileSize by remember { mutableStateOf("Size") }
+        var filePath by remember { mutableStateOf("") }
         val scope = rememberCoroutineScope()
 
         Box(Modifier.fillMaxSize()) {
@@ -76,17 +89,45 @@ fun App() {
         }
 
         Column(
-            modifier = Modifier.fillMaxSize().padding(horizontal = 40.dp),
+            modifier = Modifier.fillMaxSize().padding(40.dp),
             verticalArrangement = Arrangement.spacedBy(10.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Card(
+                shape = RoundedCornerShape(4.dp),
+                border = BorderStroke(1.dp, Color.Black),
+                modifier = Modifier.fillMaxWidth().height(100.dp)
+            ) {
+                Row(
+                    modifier = Modifier.padding(16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    Image(
+                        painter = painterResource(
+                            if (::file.isInitialized) {
+                                if (getAllFilesInResources().contains("${file.extension}.png")) "${file.extension}.png"
+                                else "file.png"
+                            } else "file.png"
+                        ),
+                        contentDescription = null
+                    )
+                    Column(modifier = Modifier.fillMaxHeight(), verticalArrangement = Arrangement.Center) {
+                        SelectionContainer { Text(text = fileName, fontSize = 20.sp) }
+                        Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
+                            SelectionContainer { Text(text = fileType) }
+                            Text(text = "|")
+                            SelectionContainer { Text(text = fileSize) }
+                        }
+                    }
+                }
+            }
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 TextField(
                     modifier = Modifier.weight(9f),
-                    value = if (::file.isInitialized) file.path else "",
+                    value = filePath,
                     onValueChange = {},
                     label = { Text("Path") }
                 )
@@ -95,6 +136,18 @@ fun App() {
                     onClick = { isFileManagerOpen = true }
                 ) {
                     Icon(imageVector = Icons.Rounded.Add, contentDescription = null)
+                }
+                IconButton(
+                    modifier = Modifier.weight(1f),
+                    onClick = {
+                        hashedOutput = ""
+                        fileSize = "Size"
+                        fileName = "File name"
+                        fileType = "Type"
+                        filePath = ""
+                    }
+                ) {
+                    Icon(imageVector = Icons.Rounded.Clear, contentDescription = null)
                 }
             }
             ComposeMenu(
@@ -129,9 +182,7 @@ fun App() {
                                     StringSelection(hashedOutput.uppercase()),
                                     null
                                 )
-                            scope.launch {
-                                snackbarHostState.showSnackbar("Copied to clipboard!")
-                            }
+                            scope.launch { snackbarHostState.showSnackbar("Copied to clipboard!") }
                         }
                     }
                 ) {
@@ -140,13 +191,13 @@ fun App() {
             }
             TextField(
                 modifier = Modifier.fillMaxWidth(),
-                value = providedHash,
-                onValueChange = { providedHash = it },
-                isError = !providedHash.equals(hashedOutput, ignoreCase = true),
+                value = comparisonHash,
+                onValueChange = { comparisonHash = it },
+                isError = !comparisonHash.equals(hashedOutput, ignoreCase = true),
                 trailingIcon = {
-                    if (hashedOutput.isNotBlank() && providedHash.isNotBlank()) {
+                    if (hashedOutput.isNotBlank() && comparisonHash.isNotBlank()) {
                         Icon(
-                            imageVector = if (providedHash.equals(hashedOutput, ignoreCase = true)) {
+                            imageVector = if (comparisonHash.equals(hashedOutput, ignoreCase = true)) {
                                 Icons.Rounded.Check
                             } else {
                                 Icons.Rounded.ErrorOutline
@@ -155,7 +206,7 @@ fun App() {
                         )
                     }
                 },
-                label = { Text("Provided Hash") }
+                label = { Text("Comparison Hash") }
             )
 
             AnimatedVisibility(
@@ -176,6 +227,10 @@ fun App() {
         if (isFileManagerOpen) {
             FileDialog { chosenFile ->
                 file = chosenFile
+                fileName = chosenFile.name
+                fileType = chosenFile.extension
+                fileSize = humanReadableByteCountBin(file.length())
+                filePath = file.path
                 isFileManagerOpen = false
                 val job = scope.launch {
                     flow {
@@ -202,9 +257,31 @@ fun App() {
     }
 }
 
-fun main() = application {
-    Window(onCloseRequest = ::exitApplication, title = "HashHash") {
-        App()
+fun getAllFilesInResources(): MutableList<String> {
+    val projectDirAbsolutePath = Paths.get("").toAbsolutePath().toString()
+    val resourcesPath = Paths.get(projectDirAbsolutePath, "/src/main/resources")
+    val list = mutableListOf<String>()
+    Files.walk(resourcesPath)
+        .filter { item -> Files.isRegularFile(item) }
+        .forEach { item -> list.add(item.fileName.toString()) }
+    return list
+}
+
+fun main() = singleWindowApplication(
+    title = "HashHash",
+    resizable = false
+) {
+    App()
+    LaunchedEffect(Unit) {
+        window.dropTarget = DropTarget().apply {
+            addDropTargetListener(object : DropTargetAdapter() {
+                override fun drop(event: DropTargetDropEvent) {
+                    event.acceptDrop(DnDConstants.ACTION_COPY)
+                    val fileName = event.transferable.getTransferData(DataFlavor.javaFileListFlavor)
+                    println(fileName)
+                }
+            })
+        }
     }
 }
 
@@ -225,3 +302,18 @@ fun FileDialog(
     },
     dispose = FileDialog::dispose
 )
+
+fun humanReadableByteCountBin(bytes: Long): String {
+    val absB = if (bytes == Long.MIN_VALUE) Long.MAX_VALUE else abs(bytes)
+    if (absB < 1024) return "$bytes B"
+    var value = absB
+    val ci: CharacterIterator = StringCharacterIterator("KMGTPE")
+    var i = 40
+    while (i >= 0 && absB > 0xfffccccccccccccL shr i) {
+        value = value shr 10
+        ci.next()
+        i -= 10
+    }
+    value *= java.lang.Long.signum(bytes).toLong()
+    return String.format("%.1f %ciB", value / 1024.0, ci.current())
+}

6fbe3854ab727a3710fe1641d7de4f7cfa773964
Russell Banks <Russell@bandev.uk>
2022-04-16 09:54:48 +0100

    Create libs.versions.toml

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,20 +11,7 @@ rootProject.name = "HashHash"
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            aurora()
-            crypto()
+            from(files("libs.versions.toml"))
         }
     }
-}
-
-fun VersionCatalogBuilder.aurora() {
-    val aurora = version("aurora", "1.1.0")
-
-    library("aurora-window","org.pushing-pixels", "aurora-window").versionRef(aurora)
-    library("aurora-component","org.pushing-pixels", "aurora-component").versionRef(aurora)
-    library("aurora-theming","org.pushing-pixels", "aurora-theming").versionRef(aurora)
-}
-
-fun VersionCatalogBuilder.crypto() {
-    library("crypto-cryptohash", "com.appmattus.crypto", "cryptohash").version("0.10.0")
 }

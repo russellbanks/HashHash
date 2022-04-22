2499d2c59ab4c1d1205c089e5bb3990b709bd39b
Russell Banks <Russell@bandev.uk>
2022-04-22 01:05:47 +0100

    Only show file extension if not the same as type

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -243,8 +243,8 @@ fun main() = auroraApplication {
                     SelectionContainer {
                         Box(modifier = Modifier.fillMaxHeight(), contentAlignment = Alignment.Center) {
                             FlowRow(mainAxisAlignment = FlowMainAxisAlignment.Center, mainAxisSpacing = 4.dp) {
-                                LabelProjection(
-                                    contentModel = LabelContentModel(text = FileUtils.getFileName(file)),
+                                LabelProjection(contentModel = LabelContentModel(
+                                    text = FileUtils.getFileName(file)),
                                     presentationModel = LabelPresentationModel(textStyle = TextStyle(fontSize = 16.sp))
                                 ).project()
                                 VerticalSeparatorProjection().project(modifier = Modifier.height(20.dp))
@@ -252,11 +252,13 @@ fun main() = auroraApplication {
                                     text = FileUtils.getFileType(file)),
                                     presentationModel = LabelPresentationModel(textStyle = TextStyle(fontSize = 16.sp))
                                 ).project()
-                                VerticalSeparatorProjection().project(modifier = Modifier.height(20.dp))
-                                LabelProjection(contentModel = LabelContentModel(
-                                    text = FileUtils.getFileExtension(file)),
-                                    presentationModel = LabelPresentationModel(textStyle = TextStyle(fontSize = 16.sp))
-                                ).project()
+                                if (FileUtils.getFileType(file) != FileUtils.getFileExtension(file)) {
+                                    VerticalSeparatorProjection().project(modifier = Modifier.height(20.dp))
+                                    LabelProjection(contentModel = LabelContentModel(
+                                        text = FileUtils.getFileExtension(file)),
+                                        presentationModel = LabelPresentationModel(textStyle = TextStyle(fontSize = 16.sp))
+                                    ).project()
+                                }
                                 VerticalSeparatorProjection().project(modifier = Modifier.height(20.dp))
                                 LabelProjection(contentModel = LabelContentModel(
                                     text = FileUtils.getFormattedBytes(file)),

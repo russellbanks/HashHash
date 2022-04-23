e7181a136e7bb10a65e01ea4b8ad7167204fc1c8
Russell Banks <Russell@bandev.uk>
2022-04-23 21:10:31 +0100

    Fix ClassNotFoundException in compiled binary

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,6 +48,7 @@ compose.desktop {
         mainClass = "MainKt"
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            includeAllModules = true
             packageName = "HashHash"
             packageVersion = "1.0.0"
         }
